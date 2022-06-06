95f7425d31a57d650a8ce60308e6c0b27d5578bd
HansHans135 <94590267+HansHans135@users.noreply.github.com>
2022-06-06 09:07:48 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ print("#####################################")
 #設定
 with open ("config.json",mode="r",encoding="utf-8") as filt:
     data = json.load(filt)
-前輟 = data["prefix"]
+PREFIX = data["prefix"]
 op_id = data["owner_id"]
 TOKEN = data["token"]
 MAX = data["max"]
@@ -29,32 +29,32 @@ client = discord.Client()
 async def on_ready():
     print('機器人已啟動，目前使用的bot：',client.user)
     status_w = discord.Status.online
-    activity_w = discord.Activity(type=discord.ActivityType.watching, name=f"{前輟}help")
+    activity_w = discord.Activity(type=discord.ActivityType.watching, name=f"{PREFIX}help")
     await client.change_presence(status= status_w, activity=activity_w)
 
 @client.event
 async def on_message(message):
 #help
-    if message.content == f"{前輟}help":
+    if message.content == f"{PREFIX}help":
         await message.delete()
-        embed = discord.Embed(title="指令清單", description=f"前輟是{前輟}", color=0x04f108)
-        embed.add_field(name=f"{前輟}help", value="操作手冊")
-        embed.add_field(name=f"{前輟}new", value="創建帳號")
-        embed.add_field(name=f"{前輟}sign", value="簽到(冷卻時間的計算為每天08:00重製)")
-        embed.add_field(name=f"{前輟}me", value="看你有多少錢")
-        embed.add_field(name=f"{前輟}money id", value="看別人有多少錢")
-        embed.add_field(name=f"{前輟}info", value="關於")
+        embed = discord.Embed(title="指令清單", description=f"前輟是{PREFIX}", color=0x04f108)
+        embed.add_field(name=f"{PREFIX}help", value="操作手冊")
+        embed.add_field(name=f"{PREFIX}new", value="創建帳號")
+        embed.add_field(name=f"{PREFIX}sign", value="簽到(冷卻時間的計算為每天08:00重製)")
+        embed.add_field(name=f"{PREFIX}me", value="看你有多少錢")
+        embed.add_field(name=f"{PREFIX}money id", value="看別人有多少錢")
+        embed.add_field(name=f"{PREFIX}info", value="關於")
         await message.channel.send(content=None, embed=embed)
 #info
-    if message.content == f"{前輟}info":
+    if message.content == f"{PREFIX}info":
         await message.delete()
-        embed = discord.Embed(title="關於", description=f"前輟是{前輟}", color=0x04f108)
+        embed = discord.Embed(title="關於", description=f"前輟是{PREFIX}", color=0x04f108)
         embed.add_field(name="每次簽到最高", value=f"{MAX}$")
         embed.add_field(name="每次簽到最低", value=f"{MIN}$")
         embed.add_field(name="關於此機器人:", value="來自開源: https://github.com/HansHans135/sign")
         await message.channel.send(content=None, embed=embed)
 #創建
-    if message.content == f"{前輟}new":
+    if message.content == f"{PREFIX}new":
       await message.delete()
       filepath = f"money/{message.author.id}.json"
       if os.path.isfile(filepath):
@@ -67,7 +67,7 @@ async def on_message(message):
           await message.channel.send(f"{message.author.mention}帳號建立完成,你目前有`{money}`元")
 
 #簽到
-    if message.content == f"{前輟}sign":
+    if message.content == f"{PREFIX}sign":
         await message.delete()
         await message.channel.send("查詢中...")
         filepath = f"money/{message.author.id}.json"
@@ -93,14 +93,14 @@ async def on_message(message):
                     json.dump(data,filt)
                 await message.channel.send(f"{message.author.mention}本日簽到獎勵:`{upmoney}`元\n你目前有`{newmoney}`元")
         else:
-            await message.channel.send(f"{message.author.mention}你沒有帳號,請用`{前輟}!new`創建一個")
+            await message.channel.send(f"{message.author.mention}你沒有帳號,請用`{PREFIX}!new`創建一個")
 #查詢
-    if message.content == f"{前輟}me":
+    if message.content == f"{PREFIX}me":
         with open (f"money/{message.author.id}.json",mode="r",encoding="utf-8") as filt:
             data = json.load(filt)
         await message.channel.send(f"{message.author.mention}你目前有`{data['money']}`元")
 
-    if message.content.startswith(f'{前輟}money'):
+    if message.content.startswith(f'{PREFIX}money'):
       await message.delete()
       tmp = message.content.split(" ",2)
       if len(tmp) == 1:
@@ -110,7 +110,7 @@ async def on_message(message):
             data = json.load(filt)
         await message.channel.send(f"{message.author.mention}id`{tmp[1]}`他現在有`{data['money']}`元")
 #give
-    if message.content.startswith(f'{前輟}give'):
+    if message.content.startswith(f'{PREFIX}give'):
         if message.author.id == int(op_id):
           await message.delete()
           tmp = message.content.split(" ",2)
@@ -119,7 +119,7 @@ async def on_message(message):
           崁入二 = tmp[1]
           #亨哥0126
           if len(tmp) == 1:
-            await message.channel.send(f"{前輟}give id 錢")
+            await message.channel.send(f"{PREFIX}give id 錢")
           else:
               filepath = f"money/{崁入一}.json"
               if os.path.isfile(filepath):
@@ -137,7 +137,7 @@ async def on_message(message):
             await message.channel.send(f"{message.author.mention}你沒有權限")
 
 #查別人
-    if message.content.startswith(f'{前輟}money'):
+    if message.content.startswith(f'{PREFIX}money'):
       await message.delete()
       tmp = message.content.split(" ",2)
       if len(tmp) == 1:
